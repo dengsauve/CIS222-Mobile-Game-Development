cb6774e82a190dd47a2c76164b3ef0cdf2406580
Dennis Sauve <dengsauve@yahoo.com>
2018-05-06 20:03:30 -0700

    Completed April 12 Lecture

---
+++ Lecture2018/Lecture2018/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float playerSpeed;
+	public bool onGround;
+	private Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 move = new Vector2 (moveHorizontal * playerSpeed, moveVertical * playerSpeed);
+		player.velocity = move;
+	}
+}
