5174ceb3e3aa72be0e08058b5d45b28532929374
Dennis Sauve <dengsauve@yahoo.com>
2018-05-12 14:03:22 -0700

    Completed Midterm Assignment

--- Eurpoa's Depths/Europa's Depths/Assets/Scripts/MovePlayer.cs
+++ Eurpoa's Depths/Europa's Depths/Assets/Scripts/MovePlayer.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour {
 
-    public float playerSpeed = 15;
-    public float jumpPower = 5;
+    public float playerSpeed = 20;
+    public float jumpPower = 6.5f;
     public float maxSpeed = 5;
+	public float laserSpeed = 750;
+	public bool isGrounded = true;
+	public GameObject laser;
 
     private float moveHorizontal, moveVertical;
     private Rigidbody2D player;
     private Vector2 move, jump;
-    public bool isGrounded = true;
+	private AudioSource laserBlast;
 
 
 	// Use this for initialization
 	void Start () {
         player = GetComponent<Rigidbody2D> ();
         jump = new Vector2(0.0f, 1.0f);
+		laserBlast = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -28,27 +32,57 @@ public class MovePlayer : MonoBehaviour {
             isGrounded = false;
         }
 
+        // Check for Laser
+		if (Input.GetKeyDown(KeyCode.F))
+        {
+			// Play Sound
+			laserBlast.Play();
+			// Spawn Laser Object
+			StartCoroutine(SpawnLaserBlast());
+        }
+
         // Get Movement Input
         moveHorizontal = Input.GetAxis("Horizontal");
-        if(isGrounded){
-            move = new Vector2(moveHorizontal * playerSpeed, 0);
-            player.AddForce(move, ForceMode2D.Force);
+		if (isGrounded)
+		{
+			move = new Vector2(moveHorizontal * playerSpeed, 0);
+		}else{
+			move = new Vector2(moveHorizontal * playerSpeed * 0.5f, 0);
+		}
 
-            if (player.velocity.x > maxSpeed)
-            {
-                move = new Vector2(maxSpeed, player.velocity.y);
-                player.velocity = move;
-            }
-            else if (player.velocity.x < (-1 * maxSpeed))
-            {
-                move = new Vector2((-1 * maxSpeed), player.velocity.y);
-                player.velocity = move;
-            }
+		player.AddForce(move, ForceMode2D.Force);
+
+
+        if (player.velocity.x > maxSpeed)
+        {
+            move = new Vector2(maxSpeed, player.velocity.y);
+            player.velocity = move;
         }
+        else if (player.velocity.x < (-1 * maxSpeed))
+        {
+            move = new Vector2((-1 * maxSpeed), player.velocity.y);
+            player.velocity = move;
+        }
+
 	}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        isGrounded = true;
+		if( collision.collider.tag == "ground")
+		    isGrounded = true; 
     }
+
+	IEnumerator SpawnLaserBlast()
+	{
+		GameObject laserInstance;
+		Vector2 currentPosition = transform.position;
+
+		laserInstance = Instantiate(laser, currentPosition, Quaternion.identity);
+
+		laserInstance.GetComponent<Rigidbody2D>().AddForce(new Vector2(laserSpeed, 0));
+		laserInstance.transform.Rotate(new Vector3(0, 0, 270));
+
+		yield return new WaitForSeconds(3);
+		Destroy(laserInstance);
+	}
 }
